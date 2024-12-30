0da2f7b6d82d49deb3fb2e8ca528969767f53f1f
princedraculla <amirtorkashvand56@gmail.com>
2024-12-30 10:48:06 +0330

    (config): load dotenv file

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
 import (
 	"github.com/joho/godotenv"
 	"log"
-	"nootebook.com/internal/gateway/http"
+	"nootebook.com/cmd"
 )
 
+func main() {
+	cmd.Execute()
+}
+
 func init() {
-	err := godotenv.Load("/home/amir/GitHub/phoonebook/.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
-}
-func main() {
-	http.ServerInit()
 }
